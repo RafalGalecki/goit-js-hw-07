f7e3d4ec69cb0bd64b71fe5ee9dc91a660b2f7fa
Rafał Gałecki <darthunter@gmail.com>
2022-11-29 14:25:13 +0100

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,13 @@
+"use strict";
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector("div.gallery");
 
-
 createGalleryItems();
+
+// gallery creation
 
 function createGalleryItems() {
   const items = galleryItems
@@ -14,14 +17,25 @@ function createGalleryItems() {
     )
     .join("");
   gallery.insertAdjacentHTML("beforeend", items);
-};
+}
 
 gallery.addEventListener("click", handleImageClick);
 
+//Listener to modal show
+
 function handleImageClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+  document.addEventListener("keydown", (ev) => {
+    if (ev.key === "Escape") {
+      instance.close();
     }
-    console.log(event.target.dataset.source);
+  });
 }

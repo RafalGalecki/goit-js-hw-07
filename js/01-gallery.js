5c6c9cf3495b904b0542ce71b0518840af6fa796
Rafał Gałecki <darthunter@gmail.com>
2022-11-29 17:36:25 +0100

    Task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,7 +13,7 @@ function createGalleryItems() {
   const items = galleryItems
     .map(
       (image) =>
-        `<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.alt}"/></a></div>`
+        `<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></div>`
     )
     .join("");
   gallery.insertAdjacentHTML("beforeend", items);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+"use strict";
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+
+const myGallery = document.querySelector("ul.gallery");
+
+createSimpleGalleryItems();
+
+function createSimpleGalleryItems() {
+  const items = galleryItems
+    .map(
+      (image) =>
+        `<a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a>`
+    )
+    .join("");
+  myGallery.insertAdjacentHTML("beforeend", items);
+}
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on();

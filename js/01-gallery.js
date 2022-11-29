fc896e90c38c48e63dcfdc20a4dcbad99204bbc3
Rafał Gałecki <darthunter@gmail.com>
2022-11-29 12:13:56 +0100

    gallery created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector("div.gallery");
+
+
+createGalleryItems();
+
+function createGalleryItems() {
+  const items = galleryItems
+    .map(
+      (image) =>
+        `<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.alt}"/></a></div>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("beforeend", items);
+};
+
+gallery.addEventListener("click", handleImageClick);
+
+function handleImageClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    
+}
